e5417cb3e10a191af452b96171f2db78e04c84f8
theodoregraham1 <117173439+theodoregraham1@users.noreply.github.com>
2023-03-07 11:45:29 +0000

    start register

--- app.py
+++ app.py
@@ -54,3 +54,6 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
+    
+
+@app.route("/register/", methods=["GET", "POST"])
