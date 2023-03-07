25846c00366d34f1bd2b205fd4a5b8589c493903
theodoregraham1 <117173439+theodoregraham1@users.noreply.github.com>
2023-03-07 11:35:02 +0000

    make login work, pulled from finance

--- app.py
+++ app.py
@@ -1,9 +1,49 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
+    """Homepage for application"""
     # User reached by GET
 
     return render_template("index.html")
+
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    """Login page for application"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 400)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Flash confirmation
+        flash("Login completed")
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
