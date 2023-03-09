009e73ff889c6a7c38184fa8558b7aac05a5802e
theodoregraham1 <117173439+theodoregraham1@users.noreply.github.com>
2023-03-09 15:06:56 +0000

    add register and connect database

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
+"""shop management application"""
 
+import sqlite3
 
 from flask import Flask, request, render_template, session, redirect, flash
 from werkzeug.security import check_password_hash, generate_password_hash
 
-
 app = Flask(__name__)
+
+# Connect to database
+con = sqlite3.connect("tutorial.db")
 
 @app.route("/", methods=["GET"])
 def index():
     """Homepage for application"""
-    # User reached by GET
-
+    
+    # User reached route via GET
     return render_template("index.html")
 
 
@@ -21,7 +25,7 @@ def login():
     # Forget any user_id
     session.clear()
 
-    # User reached route via POST (as by submitting a form via POST)
+    # User reached route via POST
     if request.method == "POST":
 
         # Ensure username was submitted
@@ -51,7 +55,7 @@ def login():
         # Redirect user to home page
         return redirect("/")
 
-    # User reached route via GET (as by clicking a link or via redirect)
+    # User reached route via GET
     else:
         return render_template("login.html")
     
@@ -59,3 +63,62 @@ def login():
 @app.route("/register/", methods=["GET", "POST"])
 def register():
     """Register page for application"""
+
+     # Forget any user_id
+    session.clear()
+
+    # User reached route via POST
+    if request.method == "POST":
+        # Register a new user
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            flash("Must provide username")
+            return redirect("/register")
+
+        # Ensure username is not taken
+        usernames = []
+        rows = db.execute("SELECT username FROM users")
+
+        for row in rows:
+            usernames.append(row["username"])
+
+        if request.form.get("username") in usernames:
+            flash("Username taken")
+            return redirect("/register")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            flash("Must provide password")
+            return redirect("/register")
+
+        # Ensure password was confirmed
+        elif not request.form.get("confirmation"):
+            flash("Must confirm password")
+            return redirect("/register")
+
+        # Ensure passwords match
+        elif request.form.get("confirmation") != request.form.get("password"):
+            flash("Passwords must match")
+            return redirect("/register")
+
+        # Generate password hash
+        hash = generate_password_hash(request.form.get("password"))
+
+        db.execute("INSERT INTO users(username, hash) VALUES (?, ?)", request.form.get("username"), hash)
+
+        # Log user in
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Flash confirmation
+        flash("Registration completed")
+
+        # Redirect user to homepage
+        return redirect("/")
+
+    # User reached route via GET 
+    else:
+        return render_template("register.html")
