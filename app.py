cdb1e97fd3e9b94219e236e3c28a96010326af24
theodoregraham1 <117173439+theodoregraham1@users.noreply.github.com>
2023-03-11 15:33:59 +0000

    add stuff

--- app.py
+++ app.py
@@ -3,13 +3,30 @@
 import sqlite3
 
 from flask import Flask, request, render_template, session, redirect, flash
+from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
+
 app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # Connect to database
 con = sqlite3.connect("tutorial.db")
 cur = con.cursor()
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 
 @app.route("/", methods=["GET"])
 def index():
